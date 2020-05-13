a27bc3910fd5fe341f8a4a905e592c2e5517a1df
quanzongwei <552114141@qq.com>
2020-05-13 10:46:00 +0800

    [fileMask]: 代码测试

--- src/test/java/com/qzw/demo/AppTest.java
+++ src/test/java/com/qzw/demo/AppTest.java
@@ -1,8 +1,12 @@
 package com.qzw.demo;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.Base64;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test for simple App.
@@ -16,5 +20,19 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    public static void main(String[] args) throws IOException {
+        RandomAccessFile raf = new RandomAccessFile("D:\\360极速浏览器下载\\member_coupon_template.xls", "rw");
+
+        Long length = raf.length();
+        byte[] data = new byte[length.intValue()];
+
+        raf.read(data);
+        System.out.println(length
+        );
+
+        String s = Base64.getEncoder().encodeToString(data);
+        System.out.println(s);
     }
 }
