2be4be6aff609b7e389231fe83f785d9dc690210
quanzongwei <quanzongwei.qzw@alibaba-inc.com>
2024-07-12 22:34:21 +0800

    1.mac and windows support 2.fix login error 3.add version comment

---
+++ src/main/java/com/qzw/filemask/component/PlatformContext.java
@@ -0,0 +1,16 @@
+package com.qzw.filemask.component;
+
+/**
+ * 平台上下文
+ *
+ * @author quanzognwei
+ * @date 2024/7/12 21:30
+ */
+public class PlatformContext {
+
+    /**
+     * 当前支持
+     * WINDOWS、MAC
+     */
+    public static String CURRENT_PLATFORM = "MAC";
+}
